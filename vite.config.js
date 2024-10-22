e31d7466201936fc84e5112ff37c2149542bddc1
Alok <Sharmaalok0785@gmail.com>
2024-10-22 15:46:19 +0530

    path is updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/4675_manish/", // Ensure the base path is correct
+  base: "/reactProfile/", // Ensure the base path is correct
   plugins: [react()],
 });
